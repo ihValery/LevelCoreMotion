863a0a474955b6dc20c4a9d8eda17764a0b4b05d
ihValery <ihValery@gmail.com>
2023-04-26 17:33:34 +0300

    Initial Commit

---
+++ LevelCoreMotion/LevelCoreMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LevelCoreMotionApp.swift
+//  LevelCoreMotion
+//
+//  Created by Валерий Игнатьев on 26.04.23.
+//
+
+import SwiftUI
+
+@main
+struct LevelCoreMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
