d1f828e173a812a709a65ff6ece47a55a729ba0e
ihValery <ihValery@gmail.com>
2023-05-01 14:47:28 +0300

    gjkjdbyrf

--- LevelCoreMotion/View/MotionIndicator.swift
+++ LevelCoreMotion/View/MotionIndicator.swift
@@ -27,30 +27,45 @@ struct MotionIndicator: View {
     var body: some View {
         VStack(spacing: 0) {
             motionValueText(xValue: x, yValue: y)
-            
-            Text("Множитель 100")
-                .padding(.bottom)
-            
-            motionValueText(xValue: x * 100, yValue: y * 100)
-            
+                        
             ZStack {
                 BigCircleView(xValue: x, yValue: y)
                 
-                SmallCircleView(xValue: x, yValue: y)
+                SmallCircleView(xValue: outsideRadius(x: x, y: y).0,
+                                yValue: outsideRadius(x: x, y: y).1)
                 
                 speckOfLight()
             }
+            
+            Text("Множитель 100")
+                .padding([.top, .bottom], 40)
+            
+            motionValueText(xValue: x * 100, yValue: y * 100)
+            
+            Text("Формула")
+                .padding([.top, .bottom], 40)
+            
+            motionValueText(xValue: outsideRadius(x: x, y: y).0,
+                            yValue: outsideRadius(x: x, y: y).1)
         }
     }
 
     //MARK: Functions
+    
+    private func outsideRadius(x: CGFloat, y: CGFloat) -> (CGFloat, CGFloat) {
+        if pow(x * 100, 2) + pow(y * 100, 2) > CGFloat(10000) {
+            return (1, 1)
+        } else {
+            return (x, y)
+        }
+    }
 
     private func motionValueText(xValue: CGFloat, yValue: CGFloat) -> some View {
         return HStack(spacing: 20) {
             Text("x = \( String(format: "%.3f", xValue) )")
-                .frame(width: 85, alignment: .leading)
+                .frame(width: 135, alignment: .center)
             Text("y = \( String(format: "%.3f", yValue) )")
-                .frame(width: 85, alignment: .leading)
+                .frame(width: 135, alignment: .center)
         }
         .foregroundColor(.gray)
         .padding(.bottom, 50)
